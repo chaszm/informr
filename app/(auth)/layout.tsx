4a831230e75d84200ec029ba9e0daf797cd24b4b
Chase <123278965+chaszm@users.noreply.github.com>
2024-07-18 15:25:41 -0400

    Added authentication with clerk + UI

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,14 +1,10 @@
-import { ClerkProvider } from "@clerk/nextjs";
-import { Inter } from "next/font/google";
-
-import "../globals.css";
-
-export const metadata = {
-  title: "Informr",
-  description: "A social media application created with Next.JS",
-};
-
-const inter = Inter({ subsets: ["latin"] });
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 
 export default function RootLayout({
   children,
@@ -17,9 +13,14 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      {" "}
       <html lang="en">
-        <body className={" ${inter.className} bg-dark-1 "}>{children}</body>
+        <body>
+          <SignedOut></SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
