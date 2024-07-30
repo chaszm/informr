8c0df1aa12b3656193845095a16943ddb72a3879
Chase <123278965+chaszm@users.noreply.github.com>
2024-07-30 18:35:30 -0400

    search and activity pages for user update

---
+++ app/(root)/activity/page.tsx
@@ -0,0 +1,25 @@
+import PostThread from "@/components/forms/PostThread"
+import ProfileHeader from "@/components/shared/ProfileHeader"
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions"
+import { currentUser } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
+import Image from "next/image"
+import { profileTabs } from "@/constants"
+import PostsTab from "@/components/shared/PostsTab"
+import UserCard from "@/components/cards/UserCard"
+
+async function Page() {
+  const user = await currentUser()
+
+  if (!user) return null
+
+  const userInfo = await fetchUser(user.id)
+
+  if (!userInfo?.onboarded) redirect("/onboard")
+
+  //getActivity
+
+  return <h1 className="head-text mb-10">Activity</h1>
+}
+
+export default Page
