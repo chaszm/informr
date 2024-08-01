e559d44376cc811ca0e62bbce38e06cdb8dfd889
Chase <123278965+chaszm@users.noreply.github.com>
2024-08-01 15:50:50 -0400

    middleware udpate

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,9 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
 
-const isPublicRoute = createRouteMatcher(["/api/webhook/clerk"])
+const isProtectedRoute = createRouteMatcher(["/dashboard(.*)", "/forum(.*)"])
 
 export default clerkMiddleware((auth, req) => {
-  if (!isPublicRoute(req)) {
-    auth().protect()
-  }
+  if (isProtectedRoute(req)) auth().protect()
 })
 
 export const config = {
